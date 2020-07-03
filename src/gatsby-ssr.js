31aa0f8c359ad1cc4fc8f670ad7d9f815eddba31
Joaquin Garcia <ing.joaquin.garcia.s@gmail.com>
2020-07-02 20:02:35 -0500

    fix: gatsby-ssr

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   if (process.env.NODE_ENV === `production`) {
+    const options = Object.keys(pluginOptions).filter(key => key !== 'plugins').map(key => pluginOptions[key])
     return setHeadComponents([
       <script
         key={`gatsby-plugin-facebook-multi-pixels`}
@@ -12,7 +13,7 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
   t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
   document,'script','https://connect.facebook.net/en_US/fbevents.js');
-  ${pluginOptions.map((opts, index) => (`
+  ${options.map((opts, index) => (`
   fbq('init', '${opts.pixelId}');
   f.${opts.alias || `fbq${index}`}=function(x,y){return fbq('trackSingle',${opts.pixelId},x,y);};
   f.${opts.alias || `fbq${index}`}Custom=function(x,y){return fbq('trackSingleCustom',${opts.pixelId},x,y);};
